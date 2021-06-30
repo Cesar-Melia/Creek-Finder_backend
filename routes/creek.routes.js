47712cbdbda901b952006b7fecf6fbfcea844ad3
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-06-30 21:28:30 +0200

    REFACTOR: separate controllers from routes

--- routes/creek.routes.js
+++ routes/creek.routes.js
@@ -2,115 +2,16 @@ const express = require('express');
 const Creek = require('../models/Creek');
 const router = express.Router();
 const { upload, uploadToCloudinary } = require('../middlewares/file.middleware');
+const { creekGet, createCreek, deleteCreek, creekEdit, creekGetById } = require('../controllers/creeks.controller');
 
-router.get('/', async (req, res, next) => {
-  try {
-    const creeks = await Creek.find();
+router.get('/', creekGet);
 
-    return res.status(200).json(creeks);
-  } catch (error) {
-    return next(error);
-  }
-});
+router.post('/create', [upload.single('img'), uploadToCloudinary], createCreek);
 
-router.post('/create', [upload.single('img'), uploadToCloudinary], async (req, res, next) => {
-  console.log('Entra hasta aqui');
+router.delete('/delete/:id', deleteCreek);
 
-  try {
-    const { name, province, type, description, lat, lng } = req.body;
+router.put('/edit/:id', [upload.single('image'), uploadToCloudinary], creekEdit);
 
-    const img = req.fileUrl ? req.fileUrl : '';
-
-    const newCreek = new Creek({
-      name,
-      img,
-      province,
-      type,
-      description,
-      lat,
-      lng,
-    });
-
-    const createdCreek = await newCreek.save();
-
-    console.log(createdCreek);
-
-    return res.status(201).json(createdCreek);
-  } catch (error) {
-    return next(error);
-  }
-});
-
-router.delete('/delete/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-
-    console.log('id: ', id);
-
-    const deletedCreek = await Creek.findByIdAndDelete(id);
-
-    let response = '';
-    if (deletedCreek) {
-      response = 'creek deleted';
-    } else {
-      response = 'creek not found';
-    }
-    return res.status(200).json(response);
-  } catch (error) {
-    return next(error);
-  }
-});
-
-// upload
-
-router.put('/edit/:id', [upload.single('image'), uploadToCloudinary], async (req, res, next) => {
-  const { id } = req.params;
-  const { name, province, type, description, lat, lng } = req.body;
-  const uploadFields = {};
-
-  if (name) {
-    uploadFields.name = name;
-  }
-
-  if (province) {
-    uploadFields.province = province;
-  }
-  if (type) {
-    uploadFields.type = type;
-  }
-  if (description) {
-    uploadFields.description = description;
-  }
-
-  if (lat) {
-    uploadFields.lat = lat;
-  }
-
-  if (lng) {
-    uploadFields.lng = lng;
-  }
-
-  try {
-    console.log(req.body);
-    const editedCreek = await Creek.findByIdAndUpdate(id, uploadFields, { new: true });
-
-    return res.status(201).json(editedCreek);
-  } catch (error) {
-    return next(error);
-  }
-});
-
-router.get('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-
-    const creek = await Creek.findById(id);
-    console.log('Creeks : ', creek);
-
-    return res.status(200).json(creek);
-  } catch (error) {
-    return next(error);
-  }
-});
+router.get('/:id', creekGetById);
 
 module.exports = router;
