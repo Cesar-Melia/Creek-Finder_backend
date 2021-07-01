65f4b3182be16026be4aa11acf441b861dd864a2
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-07-01 17:51:50 +0200

    FIX: cors bug solved

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
 
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
-const path = require("path");
+const path = require('path');
 
-const db = require("./db");
+const db = require('./db');
 const router = express.Router();
-const passport = require("passport");
+const passport = require('passport');
 
-require("./auth");
+require('./auth');
 
-const indexRoutes = require("./routes/index.routes");
-const creekRoutes = require("./routes/creek.routes");
-const userRoutes = require("./routes/user.routes");
-const authRoutes = require("./routes/auth.routes");
+const indexRoutes = require('./routes/index.routes');
+const creekRoutes = require('./routes/creek.routes');
+const userRoutes = require('./routes/user.routes');
+const authRoutes = require('./routes/auth.routes');
 
 const PORT = process.env.PORT || 3000;
 
@@ -38,21 +39,21 @@ server.use(
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-server.set("views", path.join(__dirname, "views"));
-server.set("view engine", "hbs");
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'hbs');
 
-server.use("/", indexRoutes);
-server.use("/creeks", creekRoutes);
-server.use("/users", userRoutes);
-server.use("/auth", authRoutes);
+server.use('/', indexRoutes);
+server.use('/creeks', creekRoutes);
+server.use('/users', userRoutes);
+server.use('/auth', authRoutes);
 
-server.use("*", (req, res, next) => {
-  const error = new Error("Page not found");
+server.use('*', (req, res, next) => {
+  const error = new Error('Page not found');
   return res.status(404).json(error);
 });
 
 server.use((error, req, res, next) => {
-  console.log("error--> ", error.message);
+  console.log('error--> ', error.message);
   return res.status(error.status || 500).json(error);
 });
 
