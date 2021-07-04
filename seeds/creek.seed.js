4cd4b1a334266cef9e3c7cd5206b197cd425f5dd
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-07-04 23:49:20 +0200

    FIX: seed img url solved

--- seeds/creek.seed.js
+++ seeds/creek.seed.js
@@ -6,7 +6,7 @@ const db = require('../db');
 const creekSeed = [
   {
     name: 'Cala Santo Tomas',
-    img: ['https://res.cloudinary.com/creek-finder/image/upload/v1625420315/samples/santo-tomas_1_cqecfr.jpg'],
+    img: ['https://res.cloudinary.com/creek-finder/image/upload/v1625420315/seed/santo-tomas_1_cqecfr.jpg'],
     province: 'Islas Baleares',
     type: 'arena',
     description:
@@ -18,7 +18,7 @@ const creekSeed = [
   },
   {
     name: 'Cala Turqueta',
-    img: ['https://res.cloudinary.com/creek-finder/image/upload/v1625420315/samples/santo-tomas_1_cqecfr.jpg'],
+    img: ['https://res.cloudinary.com/creek-finder/image/upload/v1625420560/seed/cala-turqueta-menorca_j7lucp.jpg'],
     province: 'Islas Baleares',
     type: 'arena',
     description: 'Cala de arena blanca, con una torre para socorristas y un bosque de pinos rodeandola',
@@ -67,6 +67,18 @@ const creekSeed = [
       'Cala de arena rojiza debido a la concentracion de arcilla. Fabuloso contraste de la arena con el azul claro del agua. De las mejores playas de Menorca, de hecho, cuenta con el privilejio de ser la portada de uno de los discos de Mike Oldfield ',
     lat: 40.05613022268097,
     lng: 4.0420949622722615,
+    timesFavourite: 0,
+    comments: [],
+  },
+  {
+    name: 'Cala Galdana',
+    img: ['https://res.cloudinary.com/creek-finder/image/upload/v1625420910/seed/cala-galdana_afab1f.jpg'],
+    province: 'Islas Baleares',
+    type: 'arena',
+    description:
+      'Cala de arena blanca y con un gran hotel justo detras, cuenta con un amplio parking para dejar el coche. Conectada con "Camí de Cavalls" para hacer rutas a otras playas cercanas',
+    lat: 39.93702057615642,
+    lng: 3.960738703558726,
     timesFavourite: 0,
     comments: [],
   },
